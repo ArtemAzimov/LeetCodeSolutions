033bddc80c62c0d34329f27c73ddcec57422468f
Artem_A <art4012@yandex.ru>
2023-09-02 09:04:06 +0300

    WIP n14

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -6,27 +6,59 @@ class Solution {
 public:
     std::string longestCommonPrefix(std::vector<std::string>& strs) {
         std::string result = "";
-        int vecsize = strs.size();
-        int stringsize = strs[0].size();
 
-        if (vecsize == 1) {return result;}
+        std::vector<int> word_sizes;
+        
+        for (const auto& word : strs){
+            word_sizes.push_back(word.size());
+        }
 
-        for (int istring = 0; istring < vecsize - 2; ++istring){
-            for (int iword = 0; iword < stringsize; ++iword){
-                if (strs[istring][iword] == strs[istring+1][iword]){ // Проход по всем?
-                    result += strs[istring][iword];
+        for (int i = 0; i < word_sizes.size() - 1; ++i){
+            if (word_sizes[i] > word_sizes[i+1]){
+                int temp = word_sizes[i+1];
+                word_sizes[i+1] = word_sizes[i];
+                word_sizes[i] = temp;
+            }
+        }
+
+        int min_size = word_sizes[0];
+
+        std::set<char> temp_set;
+        std::set<char> char_set;
+        int counter = 0;
+
+        while(counter != min_size){
+            for (const auto& word : strs){
+                if (!word.empty()){
+                    temp_set.insert(word[counter]);
+                    std::cout << "word[counter]: " << word[counter] << std::endl;
+                }
+                if (temp_set.size() == counter){
+                    std::cout << "counter: " << counter << std::endl;
+                    std::cout << "temp_set.size(): " << temp_set.size() << std::endl;
+                    result += word[counter];
+                    ++counter;
+                }
+                else {
+                    break;
                 }
             }
         }
+
         return result;
     }
 };
 
-
 int main(){
     Solution sol;
+
     std::vector<std::string> strs {"flower","flow","flight","floradar"};
     // std::vector<std::string> strs {"dogcar","racecar","car"};
+    // std::vector<std::string> strs {"sda","ggg","rrr"};
+    // std::vector<std::string> strs {""};
+    // std::vector<std::string> strs {"",""};
+    // std::vector<std::string> strs {"a"};
+    // std::vector<std::string> strs {"flower","flower","flower","flower"};
+
     std::cout << sol.longestCommonPrefix(strs);
 }
-

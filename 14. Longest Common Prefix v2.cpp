1d14e55f5ab4a00cef3741f7ce1c5b0dc6506042
Artem_A <art4012@yandex.ru>
2024-05-02 19:40:28 +0300

    Upd. Sync.

---
+++ 14. Longest Common Prefix v2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+        std::string result = "";
+        
+        for 
+        std::cout << minlen << std::endl;
+
+        return result;
+    }
+};
+
+int main(){
+    Solution sol;
+
+    std::vector<std::string> strs {"flower","flow","flight","floradar"};
+    // std::vector<std::string> strs {"dogcar","racecar","car"};
+    // std::vector<std::string> strs {"sda","ggg","rrr"};
+    // std::vector<std::string> strs {""};
+    // std::vector<std::string> strs {"",""};
+    // std::vector<std::string> strs {"a"};
+    // std::vector<std::string> strs {"flower","flower","flower","flower"};
+
+    std::cout << sol.longestCommonPrefix(strs);
+}
